943a3dfe38f746e033da78a2ef5be9fffa632e62
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2023-06-15 20:38:45 +0100

    fix: added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import routes from "./jSon/routes";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/landingPage/LandingPage.jsx";
+
+import FindHospital from "./pages/findHospital/FindHospital.jsx";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          {routes.map((route) => (
-            <Route path={route.path} element={route.element} key={route.id} />
-          ))}
+          {/* route1 */}
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <LandingPage />
+              </>
+            }
+          />
+
+          <Route
+            path="/about"
+            element={
+              <>
+                <LandingPage />
+              </>
+            }
+          />
+
+          <Route
+            path="/"
+            element={
+              <>
+                <LandingPage />
+              </>
+            }
+          />
+          <Route path="/find-hospital" element={<FindHospital />} />
         </Routes>
       </Router>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,26 @@
 import React from "react";
+import { NavLink as Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="  flex justify-between items-center py-[3vh] pl-[5.67vw] pr-[1.45vw] ">
+    <nav className="  flex justify-between items-center py-[3vh] pl-[5.67vw] pr-[1.45vw] ">
       <h1 className="text-[#08299B] font-bold text-[1.5rem] ">CareFinder</h1>
       <div className="flex items-center gap-[10vw] font-medium text-[1rem]">
-        <ul className="flex items-center gap-[4vw] text-[#1E1E1E]   ">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href={"../pages/findHospital/FindHospital"}>Find Hospital</a>
-          </li>
-        </ul>
+        <span className="flex items-center gap-[4vw] text-[#1E1E1E]   ">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/find-hospital">Find Hospital</Link>
+        </span>
         <div className="flex items-center gap-[1vw]  text-white  ">
           <button className="border-[1px] border-[#08299B] w-[7.34vw] h-[5.46vh] rounded-xl hover:opacity-90 text-[#08299B] ">
             Login
           </button>
-          <button className="bg-[#08299B] w-[7.34vw] h-[5.46vh] rounded-xl hover:bg-opacity-90  ">
+          <button className="bg-[#08299B] w-[7.34vw] h-[5.46vh] rounded-xl hover:bg-opacity-90 ">
             Sign Up
           </button>
         </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
